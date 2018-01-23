1c25af033257b05de9ea8485cd9b09f5e13fed91
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-23 20:07:54 +0100

    Change db schema, adding admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119201425) do
+ActiveRecord::Schema.define(version: 20180123190228) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180119201425) do
     t.boolean "is_male"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
