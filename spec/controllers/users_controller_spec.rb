140f20928de0539f02fd30a582406460b580d643
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-23 21:46:30 +0100

    Adding test for delete user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,4 +23,19 @@ RSpec.describe UsersController do
       end
     end
   end
+
+  describe '#destroy' do
+    context 'when user is provided' do
+      it 'delete user form database and refresh index page' do
+        user = create(:user)
+
+        delete(:destroy, params: { id: user.id })
+
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(users_path)
+        expect(flash[:success]).to eq 'User successfully destroyed'
+        expect(User.first).to eq nil
+      end
+    end
+  end
 end
