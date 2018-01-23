10c4d989f2c9e1a4a44c3083d71ce12e2c90fd94
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-23 20:59:50 +0100

    Update seeds and factories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 require 'faker'
 
-User.create!(email: 'example@mail.com',
-             password: '12345678',
+User.create!(email: 'admin@mail.com',
+             password: '1234',
              is_male: true,
+             admin: true,
              birthday: '1994-08-21')
 
 10.times do
   User.create!(email: Faker::Internet.email,
                password: Faker::Internet.password(8),
                is_male: Faker::Boolean.boolean,
+               admin: false,
                birthday: Faker::Date.birthday(18, 65))
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     email 'example@mail.com'
     password '12345678'
     is_male true
+    admin false
     birthday '1994-08-21'
   end
 end
