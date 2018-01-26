cd0a49017ff9a63e42a0f103542e7b49a5f62340
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-26 23:00:17 +0100

    Adding features test for admin, deleting users

---
+++ spec/features/admin_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin' do
+  scenario 'admin can delete user' do
+    admin = create(:user, admin: true)
+    create(:user, email: "example-user@mail.com")
+    sign_in admin
+
+    visit root_path
+    click_on 'delete icon'
+
+    expect(current_path).to eq users_path
+    expect(page).to have_content 'User successfully destroyed'
+    expect(page).not_to have_content 'example-user@mail.com'
+  end
+end
