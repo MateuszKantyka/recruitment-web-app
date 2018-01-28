f3526e88a949dc169b02400507db3e0e566bc969
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-28 14:16:16 +0100

    Adding some tests for models

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Interest do
   it 'has none to begin with' do
     expect(Interest.count).to eq 0
   end
+
+  it 'can by created with valid arguments' do
+    user = create(:user)
+    interest = Interest.new(name: 'jokes', type: 'Hobby', user_id: user.id).save
+
+    expect(Interest.count).to eq 1
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,29 @@ RSpec.describe User do
   it 'has none to begin with' do
     expect(User.count).to eq 0
   end
-  
-  it 'is valid with valid attributes' do
-    expect(create(:user)).to be_valid
+
+  it 'has a valid Factory' do
+    create(:user)
+
+    expect(User.count).to eq 1
+  end
+
+  it 'can by created with valid arguments' do
+    user = User.new(email:'example@mail.com',
+                    password:'qwerty',
+                    is_male: true,
+                    admin: false).save
+
+    expect(User.count).to eq 1
+  end
+
+  it 'can have more then one interest' do
+    user = create(:user)
+    interest = Interest.new(name: 'flying', type: 'Work', user_id: user.id).save
+    interest1 = Interest.new(name: 'jokes', type: 'Hobby', user_id: user.id).save
+
+    user.reload
+
+    expect(user.interests.count).to eq 2
   end
 end
