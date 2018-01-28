b04a5fd5cc3b9032fd8c0039eedd5206f0c52298
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-28 19:21:19 +0100

    New features test for admin

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -13,4 +13,19 @@ RSpec.feature 'Admin' do
     expect(page).to have_content 'User successfully destroyed'
     expect(page).not_to have_content 'example-user@mail.com'
   end
+
+  scenario 'admin can delete user with interests' do
+    admin = create(:user, admin: true)
+    user = create(:user, email: "example-user@mail.com")
+    create(:interest, user_id: user.id)
+    create(:interest, user_id: user.id)
+    sign_in admin
+
+    visit root_path
+    find(:xpath, "//a[@href='/users/2']").click
+
+    expect(current_path).to eq users_path
+    expect(page).to have_content 'User successfully destroyed'
+    expect(page).not_to have_content 'example-user@mail.com'
+  end
 end
