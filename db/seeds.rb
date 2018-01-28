a7827df3a39321bdd3e3be2c33308f62826dd714
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-28 18:57:14 +0100

    Adding interests to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@ User.create!(email: 'admin@mail.com',
              admin: true,
              birthday: '1994-08-21')
 
-10.times do
+2.times do
   User.create!(email: Faker::Internet.email,
                password: Faker::Internet.password(8),
                is_male: Faker::Boolean.boolean,
                admin: false,
                birthday: Faker::Date.birthday(18, 65))
+end
+
+7.times do
+  Interest.create!(name: Faker::ProgrammingLanguage.name,
+                   type: 'Work',
+                   user_id: Faker::Number.between(1,3))
 end
