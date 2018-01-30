aa5ab9ba2b091c07eb408c5c1c66fc8a67758b0b
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-01-30 20:05:56 +0100

    Correcting User_age helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def user_age(user)
-    age = Date.today.year - user.birthday.year
-    age - 1 if Date.today < user.birthday + age.years
+    now = Time.now.utc.to_date
+    now.year - user.birthday.year - ((now.month > user.birthday.month || (now.month == user.birthday.month && now.day >= user.birthday.day)) ? 0 : 1)
   end
 end
