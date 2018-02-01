4773de4f0acf2b1afccb2936e1b720662117acbe
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-01 20:40:13 +0100

    Adding routes for admin index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :destroy]
-  get '/admin_panel', to: 'users#admin_panel'
+  resources :admins, only: :index
   root 'users#index'
 end
