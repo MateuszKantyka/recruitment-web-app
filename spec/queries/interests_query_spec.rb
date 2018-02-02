f3ef81291400da146fbebad8be539e8a8ec034f2
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-02 19:44:23 +0100

    Refactoring with rubocop

--- spec/queries/interests_query_spec.rb
+++ spec/queries/interests_query_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe InterestsQuery do
   describe '#call' do
-    it 'returns the current number interests type "health", with name
-        beginning with ,,cosm'' choosen by women aged between 20 and 30' do
-      user_nr_1 = create(:user, is_male: false, birthday: Date.current-25.year)
-      create(:interest, user_id: user_nr_1.id, type: 'health', name: 'cosmos')
-      create(:interest, user_id: user_nr_1.id, type: 'health', name: 'cosmetics')
-      user_nr_2 = create(:user, is_male: false, birthday: Date.current-22.year)
-      create(:interest, user_id: user_nr_2.id, type: 'health', name: 'cosmetics')
-      create(:interest, user_id: user_nr_2.id, type: 'work', name: 'ruby')
+    it "returns the current number interests type 'health',
+    with name beginning with 'cosm' choosen by women aged between 20 and 30" do
+      user_nr1 = create(:user, is_male: false, birthday: Date.current - 25.year)
+      create(:interest, user_id: user_nr1.id, type: 'health', name: 'cosmos')
+      create(:interest, user_id: user_nr1.id, type: 'health', name: 'cosmetics')
+      user_nr2 = create(:user, is_male: false, birthday: Date.current - 22.year)
+      create(:interest, user_id: user_nr2.id, type: 'health', name: 'cosmetics')
+      create(:interest, user_id: user_nr2.id, type: 'work', name: 'ruby')
 
       expect(InterestsQuery.call).to eq 3
     end
