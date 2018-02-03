733ebb19f2e92e9138d5c494603c5e7806fa4b9c
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-03 17:57:22 +0100

    Adding action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,38 @@ class UsersController < ApplicationController
     @users = @q.result(distinct: true)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.password = 'secret'
+    @user.admin = false
+    if @user.save
+      redirect_to admins_path
+      flash[:success] = 'User created'
+    else
+      flash.now[:danger] = 'Correct the field'
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'User updated'
+      redirect_to admins_path
+    else
+      flash.now[:danger] = 'Correct the field'
+      render 'edit'
+    end
+  end
+
   def destroy
     authorize User
     user = User.find(params[:id])
@@ -22,5 +54,9 @@ class UsersController < ApplicationController
 
   def require_login
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :is_male, :birthday)
   end
 end
