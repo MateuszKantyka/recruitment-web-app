f7cd5f6bf79a5971443c872de15443fb01ae74f5
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-03 14:03:24 +0100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
 
   def self.to_csv
     attributes = %w[id email gender age interests_list]
-
     CSV.generate(headers: true) do |csv|
       csv << attributes
       all.each { |user| csv << attributes.map { |attr| user.public_send(attr) } }
