8c44aff6121ed70e1930567ffcbad94e728ef4e0
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-03 09:12:44 +0100

    Change fields in csv file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  include UsersHelper
   devise :database_authenticatable
   has_many :interests, dependent: :delete_all
   validates :email, uniqueness: true
 
   def self.to_csv
-   attributes = %w{id email is_male birthday interests}
+   attributes = %w{id email gender age interests_list}
 
    CSV.generate(headers: true) do |csv|
      csv << attributes
@@ -12,5 +13,17 @@ class User < ApplicationRecord
        csv << attributes.map{ |attr| user.send(attr) }
      end
    end
+ end
+
+ def gender
+   is_male ? 'male' : 'female'
+ end
+
+ def age
+   user_age(self)
+ end
+
+ def interests_list
+   self.interests.pluck(:name).join(', ')
  end
 end
