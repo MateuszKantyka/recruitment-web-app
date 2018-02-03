a51043a431f3934b7d7e234306cacda61275b60d
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-03 10:57:58 +0100

    Change user_id: to user: in query spec

--- spec/queries/interests_query_spec.rb
+++ spec/queries/interests_query_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe InterestsQuery do
     it "returns the current number interests type 'health',
     with name beginning with 'cosm' choosen by women aged between 20 and 30" do
       user_nr1 = create(:user, is_male: false, birthday: Date.current - 25.year)
-      create(:interest, user_id: user_nr1.id, type: 'health', name: 'cosmos')
-      create(:interest, user_id: user_nr1.id, type: 'health', name: 'cosmetics')
+      create(:interest, user: user_nr1, type: 'health', name: 'cosmos')
+      create(:interest, user: user_nr1, type: 'health', name: 'cosmetics')
       user_nr2 = create(:user, is_male: false, birthday: Date.current - 22.year)
-      create(:interest, user_id: user_nr2.id, type: 'health', name: 'cosmetics')
-      create(:interest, user_id: user_nr2.id, type: 'work', name: 'ruby')
+      create(:interest, user: user_nr2, type: 'health', name: 'cosmetics')
+      create(:interest, user: user_nr2, type: 'work', name: 'ruby')
 
       expect(InterestsQuery.call).to eq 3
     end
