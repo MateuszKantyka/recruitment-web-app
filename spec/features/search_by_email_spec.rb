9672e25cc463583ffc928421a950b68b9eaa5fda
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-04 17:36:39 +0100

    Correction features spec for search by email

--- spec/features/search_by_email_spec.rb
+++ spec/features/search_by_email_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'Search' do
   scenario 'user can set scope the listing by inputing the email' do
-    user = (:user, email: 'example@mail.com')
-    (:user, email: 'example_mail_2@mail.com')
-    (:user, email: 'example_mail_3@mail.com')
+    user = create(:user, email: 'example@mail.com')
+    create(:user, email: 'example_mail_2@mail.com')
+    create(:user, email: 'example_mail_3@mail.com')
     sign_in user
 
     visit root_path
-    fill_in :search_field_email, with: 'example@mail.com'
+    fill_in :q_email_cont, with: 'example@mail.com'
     click_on 'Search'
 
     expect(page).to have_content 'example@mail.com'
