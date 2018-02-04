87933e66027832ed4e3dd504ed8c976375e39886
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-04 13:05:52 +0100

    Adding require csv

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
+require 'csv'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
