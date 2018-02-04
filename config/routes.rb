cd196b3bf435d2c593497efefc4aaaf8cb105150
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-04 17:04:50 +0100

    Change scope of user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :new, :create, :edit, :update, :delete]
-  resources :admins, only: :index
+  resources :admins, only: :index 
   root 'users#index'
 end
