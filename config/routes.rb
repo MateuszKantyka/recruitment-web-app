8a176c6cfc2bcdd0e1efdc213e7851d9ad686bae
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-05 13:13:46 +0100

    Adding to routes filecustom action form users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  post '/users/:id',  to: 'users#send_mail'
   resources :users, only: [:index, :destroy]
   resources :admins, only: :index
   root 'users#index'
