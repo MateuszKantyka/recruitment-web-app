dc0a723fbeb31b320a60f137a1c48ca693e933a7
mateusz.kantyka@hotmail.com <mateusz.kantyka@hotmail.com>
2018-02-09 18:48:32 +0100

    Add new expect

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Interest do
   it 'can have type only from predefined category: health, hobby, work' do
     user = create(:user)
-    Interest.new(name: 'Ruby', type: 'work', user_id: user.id).save
-    Interest.new(name: 'Jokes', type: 'hobby', user_id: user.id).save
-    Interest.new(name: 'Football', type: 'health', user_id: user.id).save
-
+    Interest.create(name: 'Ruby', type: 'work', user_id: user.id)
+    Interest.create(name: 'Jokes', type: 'hobby', user_id: user.id)
+    Interest.create(name: 'Football', type: 'health', user_id: user.id)
+    
+    expect { Interest.create(name: 'C++', type: 'programing', user_id: user.id) }.to raise_error(ArgumentError)
     expect(user.interests.count).to eq 3
   end
 end
